baac61c3c066d6f80b4628a3bb055c49249fa320
umesh sharma <109520296+Umeshsharma0707@users.noreply.github.com>
2023-03-28 23:35:40 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,27 @@
+package basic;
+
+public class Array {
+			public static void main(String[] args) {
+				// classroom of 500 students - you have to store marks of these 500 students
+				
+				// there are three ways to declare array in java
+				
+			//	int []marks =  new int[5];    // way one declaration and memory allocation
+				
+				// declaration and then memory allocation
+				// int []marks;  
+				// marks = new int[5];
+				
+			/*	marks[0]=100;
+				marks[4]=86;
+				marks[1]=45;
+				marks[2]=12; */
+				
+				// declaration, memory allocation and initialization together
+				int marks[] = {0,1,2,3,4,};
+				
+				
+				System.out.println(marks[4]);
+				
+			}
+}
